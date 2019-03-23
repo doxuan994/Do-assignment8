8d0a4677b2043b4b8df53da4cb79dc4f5004ef96
Xuan Do <doxuancntp@gmail.com>
2019-03-22 22:25:51 -0400

    Commit.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Move sidebar on scroll
+let sidebar = document.getElementById('sidebar');
+let footer = document.getElementById('site-footer');
+let offsetTop = sidebar.offsetTop;
+
+let windowInnerWidth = window.innerWidth;
+console.log(windowInnerWidth);
+
+
+window.addEventListener('scroll', function() {
+
+
+
+    let footerClientY = footer.getBoundingClientRect().top;
+    let footerClientHeight = footer.clientHeight;
+
+    // Stop adding margin to sidebar when users reach the end of the content.
+    let stopAddMargin = footerClientY - footerClientHeight;
+
+
+
+
+    if (windowInnerWidth >= 1200 && pageYOffset > offsetTop  && stopAddMargin > 0) {
+
+        let distance = pageYOffset - offsetTop + 32;
+
+        sidebar.style.top = distance + 'px';
+
+    }
+
+
+
+}, false);
