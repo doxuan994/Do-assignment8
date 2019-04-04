df758fbc3f0a6dbb94e7e41244a664352af0cf1a
Xuan Do <doxuancntp@gmail.com>
2019-04-04 19:29:56 -0400

    Commit.

--- js/main.js
+++ js/main.js
@@ -4,30 +4,14 @@ let footer = document.getElementById('site-footer');
 let offsetTop = sidebar.offsetTop;
 
 let windowInnerWidth = window.innerWidth;
-console.log(windowInnerWidth);
-
-
 window.addEventListener('scroll', function() {
-
-
-
     let footerClientY = footer.getBoundingClientRect().top;
     let footerClientHeight = footer.clientHeight;
 
     // Stop adding margin to sidebar when users reach the end of the content.
     let stopAddMargin = footerClientY - footerClientHeight;
-
-
-
-
     if (windowInnerWidth >= 1200 && pageYOffset > offsetTop  && stopAddMargin > 0) {
-
         let distance = pageYOffset - offsetTop + 32;
-
         sidebar.style.top = distance + 'px';
-
     }
-
-
-
 }, false);
